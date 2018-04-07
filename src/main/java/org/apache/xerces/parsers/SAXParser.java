26f27a0c314c2a5d3969687d364340cea9b1ccc9
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-04-07 10:51:01 +0200

    xml native api

---
+++ src/main/java/org/apache/xerces/parsers/SAXParser.java
@@ -0,0 +1,5 @@
+package org.apache.xerces.parsers;
+
+public class SAXParser extends com.sun.org.apache.xerces.internal.parsers.SAXParser {
+
+}
---
+++ src/main/java/org/apache/xerces/xni/XNIException.java
@@ -0,0 +1,76 @@
+package org.apache.xerces.xni;
+
+/**
+ * This exception is the base exception of all XNI exceptions. It
+ * can be constructed with an error message or used to wrap another
+ * exception object.
+ * <p>
+ * <strong>Note:</strong> By extending the Java
+ * <code>RuntimeException</code>, XNI handlers and components are
+ * not required to catch XNI exceptions but may explicitly catch
+ * them, if so desired.
+ *
+ * @author Andy Clark, IBM
+ *
+ * @version $Id: XNIException.java,v 1.6 2010-11-01 04:40:19 joehw Exp $
+ */
+public class XNIException
+    extends RuntimeException {
+
+    /** Serialization version. */
+    static final long serialVersionUID = 9019819772686063775L;
+
+    //
+    // Data
+    //
+
+    /** The wrapped exception. */
+    private Exception fException;
+
+    //
+    // Constructors
+    //
+
+    /**
+     * Constructs an XNI exception with a message.
+     *
+     * @param message The exception message.
+     */
+    public XNIException(String message) {
+        super(message);
+    } // <init>(String)
+
+    /**
+     * Constructs an XNI exception with a wrapped exception.
+     *
+     * @param exception The wrapped exception.
+     */
+    public XNIException(Exception exception) {
+        super(exception.getMessage());
+        fException = exception;
+    } // <init>(Exception)
+
+    /**
+     * Constructs an XNI exception with a message and wrapped exception.
+     *
+     * @param message The exception message.
+     * @param exception The wrapped exception.
+     */
+    public XNIException(String message, Exception exception) {
+        super(message);
+        fException = exception;
+    } // <init>(Exception,String)
+
+    //
+    // Public methods
+    //
+
+    /** Returns the wrapped exception. */
+    public Exception getException() {
+        return fException;
+    } // getException():Exception
+
+    public Throwable getCause() {
+       return fException;
+    }
+} // class QName
